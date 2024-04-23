097502c884c682f6b1997613b37ed22f1db79816
arminamiridarban <arminamiridarban@gmail.com>
2024-04-23 14:09:03 +0200

    css bug fixed

--- digitalbusiness/settings.py
+++ digitalbusiness/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5!u&v9e+k(+237=ud324&q2$&s%hc4ncgp3wv)no5=g69ojy0!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
